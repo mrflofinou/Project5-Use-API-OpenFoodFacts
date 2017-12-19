dba1938c44cf48be6982b697e4a3c97f0b796738
Florian <florian.lescuyer@gmail.com>
2017-12-19 16:32:21 +0100

    add the creation of the table categories and dostrings

--- database.py
+++ database.py
@@ -1,19 +1,47 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
+"""
+This file create the tables of the database with the ORM SQLAlchemy
+The tabeles are:
+    - categotries
+"""
+
+
 from sqlalchemy import create_engine, Column, Integer, String, Index, Text
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 import settings as s
-#from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('mysql+pymysql://s.username:s.password@localhost/test_projet05')
-#Session = sessionmaker(bind=engine)
+
+engine = create_engine('mysql+pymysql://{}:{}@localhost/test_projet05'.format(s.username, s.password))
+Session = sessionmaker(bind=engine)
+session = Session()
 Base = declarative_base()
 
 class Category(Base):
+    """
+    This class is for create the database table of the categories of food
+    """
     __tablename__ = 'categories'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(150), nullable=False)
     description = Column(Text)
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+# Schema mapping to create the tables
+Base.metadata.create_all(engine)
+
+category_fruits = Category("Fruits", "Que des bons fruits")
+category_biscuits = Category("biscuits", "Tous ce qui croustille")
+
+# Add the modification in database
+session.add(category_fruits)
+session.add(category_biscuits)
+session.commit()
+session.close()
