c36856db46f9eee9340a757efed260f80c0e8273
Florian <florian.lescuyer@gmail.com>
2017-12-21 12:03:50 +0100

    In creation of the engine: add the charset utef8, in table declaration: add storage engine

--- database.py
+++ database.py
@@ -9,13 +9,14 @@ The tabeles are:
 
 
 from sqlalchemy import create_engine, Column, Integer, String, Index, Text
+from sqlalchemy.dialects.mysql import INTEGER
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 import settings as s
 
-
-engine = create_engine('mysql+pymysql://{}:{}@localhost/test_projet05'.format(s.username, s.password))
+# creation of engine with our database (here mysql) and the DBAPI (here pymysql). DON'T FORGET THE CHARSET UTF8 !
+engine = create_engine('mysql+pymysql://{}:{}@localhost/test_projet05?charset=utf8&use_unicode=0'.format(s.username, s.password))
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
 
@@ -25,8 +26,9 @@ class Category(Base):
     """
     __tablename__ = 'categories'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(INTEGER(unsigned=True), primary_key=True)
     name = Column(String(150), nullable=False)
+    mysql_engine='InnoDB',
 
     def __init__(self, name):
         self.name = name
