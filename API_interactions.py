04c997c8b6017c644aa7d8b0bac202bbca0eac7a
Florian <florian.lescuyer@gmail.com>
2018-01-04 12:06:12 +0100

    Creation of file to make requests to th API

---
+++ API_interactions.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+"""
+This file allows to make the differents requests to the API of Open Food Facts
+"""
+
+import json
+import requests
+
+# Make GET request to the API
+resp = requests.get('https://fr.openfoodfacts.org/categories.json')
+data = resp.json()
+print(resp.status_code)
+
+# Save the JSON data in a file
+with open("categories.json", "w") as categories:
+    json.dump(data, categories, ensure_ascii=False)
