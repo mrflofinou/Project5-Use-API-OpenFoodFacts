f76f94b107831319df617ab8eae739eaf45583e8
Florian <florian.lescuyer@gmail.com>
2018-01-08 11:28:33 +0100

    Add the lines to create the engine and session wuth the ORM and add insertion ofproducts in db

--- tables_creation.py
+++ tables_creation.py
@@ -2,28 +2,51 @@
 # coding: utf-8
 
 """
-This file create the tables of the database with the ORM SQLAlchemy
+This file creates the tables of the database with the ORM SQLAlchemy
 """
 
 import json
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-from database import engine, Base, Session, Category
+from database import Base, Category, Product
+import settings as s
 
-
+# Creation of engine with our database (here mysql) and the DBAPI (here pymysql)
+# DON'T FORGET THE CHARSET UTF8 !
+engine = create_engine('mysql+pymysql://{}:{}@localhost/test_projet05?charset=utf8&use_unicode=0'.format(s.username, s.password))
+Session = sessionmaker(bind=engine)
 # Creation of a session to commnicate with the database
 session = Session()
 # Schema mapping to create the tables
 Base.metadata.create_all(engine)
 
 # Creation of categories
-i = 0
-for cat in json.load(open('categories.json')):
-    category = Category(cat['category'])
+i=0
+categories = json.load(open("categories.json"))
+for elmt in categories["tags"]:
+    category = Category(elmt["name"])
     # Add the modification in database
     session.add(category)
     i += 1
-    if i == 2:
+    if i == 1:
         break
 
+category1 = json.load(open("category1.json"))
+for elmt in category1["products"]:
+    product = Product(elmt["product_name"], category)
+    # Add the modification in database
+    session.add(category)
+
+#Save the modifications
 session.commit()
+# Close the session
 session.close()
+
+# i=0
+# compter = json.load(open("categories.json"))
+# for elmt in compter["tags"]:
+#     if elmt["products"] < 100 and elmt["products"] > 90:
+#         print(elmt["name"])
+#         i += 1
+# print(i)
