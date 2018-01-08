4d21cd867e8798d426d7c1b0c6e26c771467ab9a
Florian <florian.lescuyer@gmail.com>
2018-01-08 11:22:49 +0100

    Add get request for the products of the first category

--- API_interactions.py
+++ API_interactions.py
@@ -11,8 +11,14 @@ import requests
 # Make GET request to the API
 resp = requests.get('https://fr.openfoodfacts.org/categories.json')
 data = resp.json()
-print(resp.status_code)
+#print(resp.status_code)
 
 # Save the JSON data in a file
 with open("categories.json", "w") as categories:
     json.dump(data, categories, ensure_ascii=False, indent=4, sort_keys=True)
+
+# resp = requests.get('https://fr.openfoodfacts.org/categorie/aliments-et-boissons-a-base-de-vegetaux.json')
+# data = resp.json()
+#
+# with open("category1.json", "w") as category1:
+#     json.dump(data, category1, ensure_ascii=False, indent=4, sort_keys=True)
