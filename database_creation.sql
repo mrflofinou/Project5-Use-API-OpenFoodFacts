24d61c3a0550f35a8713c130d97d7f9ba25ef3c5
Florian <florian.lescuyer@gmail.com>
2018-01-29 16:05:37 +0100

    Add creation of table 'substitutes'

--- database_creation.sql
+++ database_creation.sql
@@ -13,14 +13,24 @@ ENGINE=INNODB;
 
 CREATE TABLE products (
   id INT UNSIGNED AUTO_INCREMENT,
-  id_category INT UNSIGNED,
+  id_category BIGINT UNSIGNED,
   name VARCHAR(150) NOT NULL,
-  ingredients VARCHAR(500),
   magasin VARCHAR(100),
   url VARCHAR(200),
   PRIMARY KEY(id),
   CONSTRAINT fk_id_category
     FOREIGN KEY (id_category)
     REFERENCES categories (id)
+)
+ENGINE=INNODB;
+
+CREATE TABLE substitutes (
+  id INT UNSIGNED AUTO_INCREMENT,
+  id_substitute BIGINT UNSIGNED,
+  id_product_substituted BIGINT UNSIGNED,
+  nom VARCHAR(150) NOT NULL,
+  magasin VARCHAR(100),
+  url VARCHAR(200),
+  PRIMARY KEY(id)
 )
 ENGINE=INNODB;
