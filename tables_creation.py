e78f72adc6625d3be6da9dd117618e479a4bc9ae
Florian <florian.lescuyer@gmail.com>
2018-01-31 10:49:02 +0100

    Modifications to have a code clearer

--- tables_creation.py
+++ tables_creation.py
@@ -7,6 +7,7 @@ This file creates the tables of the database with the ORM SQLAlchemy
 
 import json
 import requests
+from sqlalchemy.sql.expression import insert
 
 from database import Category, Product
 from base import Base, Session, engine
@@ -14,14 +15,15 @@ from base import Base, Session, engine
 
 # Creation of a session to commnicate with the database
 session = Session()
-# Drop the tables
-Base.metadata.drop_all(engine)
 # Schema mapping to create the tables
 Base.metadata.create_all(engine)
 
 def get_from_api(url):
+    """
+    Function to get data from the API
+    """
     # Make GET request to the API
-    resp = requests.get((url+".json"))
+    resp = requests.get(url+".json")
     data = resp.json()
     return data
 
@@ -29,18 +31,27 @@ categories = get_from_api("https://fr.openfoodfacts.org/categories")
 i = 0;
 for elmt in categories["tags"]:
     # No nutrition grades for beers and wines
-    if elmt["products"] <= 2000 or elmt["name"] != "Bières" or elmt["name"] != "Vins":
-        category = Category(elmt["name"])
-        # Add the modification in the session
-        session.add(category)
+    if elmt["products"] < 2000:
+        categories = insert(Category).prefix_with("IGNORE")
+        cat_values = {'name': elmt["name"]}
+        session.execute(categories, cat_values)
         i += 1
         # Get products from page 1 to page 5
         for j in range(1, 6):
             products = get_from_api(elmt["url"]+"/{}".format(j))
             for elemt in products["products"]:
-                product = Product(name=elemt["product_name"], id_product=elemt["_id"], category=category, url=elemt["url"], nutriscore=elemt.get("nutrition_grade_fr"), store=elemt.get("stores"))
-                # Add the modification in the session
-                session.add(product)
+                # Insert data in database
+                product = insert(Product).prefix_with("IGNORE")
+                product_values = {
+                'name': elemt["product_name"],
+                'id_product': elemt["_id"],
+                'id_category': i,
+                'store': elemt.get("stores"),
+                'nutriscore': elemt.get("nutrition_grade_fr", "e"),
+                'url': elemt["url"]
+                }
+                session.execute(product, product_values)
+                # session.add(product)
         if i == 30:
             break
 
