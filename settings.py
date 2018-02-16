8e2fca2c03c8be1298603b45bfc52b5840ba9cf6
Florian <florian.lescuyer@gmail.com>
2018-02-16 17:13:03 +0100

    seetings.py is for configure the database

---
+++ settings.py
@@ -0,0 +1,10 @@
+"""This file contains username and password for the connexion to the database"""
+
+username = 'florian'
+password = '01032011'
+
+# Default I have chosen to display 30 categories
+# You can modify this limit if you want
+categories_limit = 30
+# You can change the name of the database if you want
+database_name = 'openfoodfacts'
